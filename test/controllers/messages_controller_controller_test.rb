d9f592132a50fdb383663a7f9e3ec0d0dba75698
bbz8 <tech-camp@1-084.local>
2018-03-30 16:32:22 +0900

    delete_test&coffee
    不要なファイルの削除

--- test/controllers/messages_controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MessagesControllerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get messages_controller_index_url
-    assert_response :success
-  end
-
-end
