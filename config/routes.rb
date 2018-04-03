10b757083311a9955de8813317ae8f2c2ff9e92a
bbz8 <tech-camp@1-084.local>
2018-04-03 13:24:01 +0900

    group/new
    新規グループ作成画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   root 'user#index'
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
