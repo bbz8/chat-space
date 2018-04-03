3a5cab031ec893e8c4a46e5abee8728a252e82ae
bbz8 <tech-camp@1-084.local>
2018-04-03 19:38:10 +0900

    side_bar
    グループをサイドバーに表示した。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   root 'user#index'
-  root 'groups#index'
+  root 'group#index'
+  root 'group#new'
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups, only: [:new, :create, :edit, :update] do
