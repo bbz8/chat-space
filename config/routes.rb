c68b38cfbe50fc0713386cc34fa9db1ca5a35e6f
bbz8 <tech-camp@1-084.local>
2018-04-03 20:21:44 +0900

    group_edit
    グループの編集機能を実装した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   root 'user#index'
-  root 'group#index'
+  root 'groups#index'
   root 'group#new'
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
