a3ec872e87af2b7eb5b9ce08a07f16b83bb35b59
bbz8 <tech-camp@1-084.local>
2018-04-17 11:32:55 +0900

    html_in_js
    jsファイル内のHTML記述の修正。
    非同期通信id名を修正。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,20 @@
 $(function(){
   function buildHTML(message){
-    var html = `<p>
-                  <strong>
-                    <a href=${message.id}>${message.user_name}</a>
-                    ：
-                  </strong>
-                  ${message.content}
-                </p>`
+    var html = `<ul>
+                  <ul>
+                    <li class="name">
+                    </li>
+                    <li class="date">
+                    </li>
+                    <li class="lower-meesage">
+                    </li>
+                    <p class="lower-message__content">
+                    </p>
+                  </ul>
+                </ul>`
     return html;
   }
-  $('#create_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -25,9 +30,12 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html)
       $('.form__message').val('')
+      $('.content-body').animate({scrollTop: $('.content-body')[0].scrollHeight})
     })
     .fail(function() {
       alert('error');
-    });
-  });
+    })
+  return false;
+  })
 });
+
