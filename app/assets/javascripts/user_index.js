9e8137c1686b59bc3500fb1fdfe0715a1e3a344c
bbz8 <tech-camp@1-084.local>
2018-04-20 16:25:33 +0900

    append_html
    検索後に追加されるHTMLの記述を修正。

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,3 +1,14 @@
+$(function() {
+  let inputName = $("#user-search-field")
+  let searchName = $("#user-search-result");
+  function appendUser(user)  {
+      let html =`<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                 </div>`
+    searchName.append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
 
