85143bf1dafe4a3bd9a51fbdc5d01a3c429b3955
bbz8 <tech-camp@1-084.local>
2018-04-23 18:53:53 +0900

    group_controller_blank_line
    空行の追加と削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,18 @@
 class GroupsController < ApplicationController
+
   before_action :set_group, only: [:edit, :update]
+
   def index
   end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
-  def edit
 
+  def edit
   end
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -17,6 +21,7 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -26,10 +31,10 @@ class GroupsController < ApplicationController
   end
 
   private
-
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
   def set_group
     @group = Group.find(params[:id])
   end
